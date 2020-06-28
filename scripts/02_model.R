f78ba00d0fe86884980abad3aa5f338fb792588a
Subhayan Mukerjee <mail@subhayan.com>
2020-06-28 00:50:51 -0500

    model 1

--- scripts/02_model.R
+++ scripts/02_model.R
@@ -4,28 +4,113 @@ library(igraph)
 ##################################################
 # model paramters
 
-n1 <- 50 # number of websites in the universe
-n2 <- 1000 # number of members of the audiences
-n3 <- 10 # number of websites each person visits
-n4 <- 5 # number ot types of websites / people
+# n1: number of websites in the universe
+# n2: number of members of the audiences
+# n3: number of websites each person visits
+# n4: number ot types of websites / people
+
+# tau:
+# each person visits a total of n3 websites
+# which websites they visit depends on a tuning parameter tau (which controls their randomness)
+# when tau is 0 they can only visit websites whose outlet_id == their own p_id
+# when tau is 1 they can visit any website
+# when tau is 0.5, half of the websites they visit can be any website, the other half have to be restricted to those whose outlet_id == p_id
+
 
 ##################################################
 
-outlet_ids <- 1:n1
-p_ids <- 1:n2
-types <- LETTERS[1:n4]
+get_simulated_network <- function(n1, n2, n3, n4, tau) {
+  outlet_ids <- 1:n1
+  p_ids <- 1:n2
+  types <- LETTERS[1:n4]
+  
+  outlets_tbl <- tibble(
+    outlet_id = outlet_ids,
+    outlet_name = paste("O", outlet_ids, sep = "_"),
+    outlet_type = rep(types, each = n1/n4)
+  )
+  
+  audience_tbl <- tibble(
+    p_id = p_ids,
+    p_name = paste("P", p_ids, sep = ""),
+    p_type = rep(types, each = n2/n4)
+  )
+  
+  
+  audience_el <- NULL
+  
+  # loop over each person
+  for(p in 1:1000) {
+    
+    # when tau is 0 all of their choices are selective
+    # when tau is 1 all of their choices are random
+    selective_choices_allowed <- n3 - tau * n3
+    random_choices_allowed <- tau * n3
+    
+  
+    selective_chosen_outlets <- outlets_tbl %>%               # only from
+      filter(outlet_type == audience_tbl$p_type[p]) %>%       # those outlets where outlet_type == p_type
+      pull(outlet_id) %>%                                     #
+      sample(selective_choices_allowed, replace = TRUE)       # sample the number of selective outlets allowed
+    
+    random_chosen_outlets <- outlets_tbl %>%                  # from
+      pull(outlet_id) %>%                                     # all outlets in the universe
+      sample(random_choices_allowed, replace = TRUE)          # sample the number of random outlets allowed
+    
+    all_chosen_outlets <- c(selective_chosen_outlets,
+                            random_chosen_outlets)
+    
+    # build the edge-list for the audience network
+    audience_el <- audience_el %>%
+      rbind(
+        tibble(
+          p_name = paste("P", rep(p, n3), sep = "_"),             # one column is the p_id
+          outlet_name = paste("O", all_chosen_outlets, sep = "_") # second column is the outlet_id
+        )
+      )
+  }
+  
+  audience_g <- graph_from_data_frame(audience_el, directed = F)
+  V(audience_g)$type <- substr(V(audience_g)$name, 1, 1) == "O"
+  
+  projection_graphs <- bipartite_projection(audience_g, multiplicity = TRUE)
+  
+  outlet_projection <- projection_graphs$proj2
+  
+  # V(outlet_projection)$type <- unlist(
+  #                                 lapply(V(outlet_projection)$name,
+  #                                   FUN = function(x) { 
+  #                                     outlets_tbl %>%
+  #                                       filter(outlet_name == x) %>% 
+  #                                       pull(outlet_type)
+  #                                     }
+  #                                   )
+  #                               )
+  
+  V(outlet_projection)$type <- V(outlet_projection)$name %>%
+                                  lapply(FUN = function(x) { 
+                                          outlets_tbl %>%
+                                          filter(outlet_name == x) %>% 
+                                          pull(outlet_type)
+                                        }
+                                    ) %>%
+                                  unlist()
+  
+  # colrs <- c("gray50", "tomato", "gold", "purple", "cyan")
+  V(outlet_projection)$color <- ifelse(V(outlet_projection)$type == "A", "gray50",
+                                       ifelse(V(outlet_projection)$type == "B", "tomato",
+                                              ifelse(V(outlet_projection)$type == "C", "gold",
+                                                     ifelse(V(outlet_projection)$type == "D", "olivedrab4",
+                                                            "cyan"))))
+  
+  return(outlet_projection)
+  
+}
 
-outlets_tbl <- tibble(
-  outlet_id = outlet_ids,
-  outlet_name = paste("O", outlet_ids, sep = "_"),
-  outlet_type = rep(types, each = n1/n4)
-)
-
-audience_tbl <- tibble(
-  p_id = p_ids,
-  p_name = paste("P", p_ids, sep = ""),
-  p_type = rep(types, each = n2/n4)
-)
+n1 <- 50 # number of websites in the universe
+n2 <- 1000 # number of members of the audiences
+n3 <- 1000 # number of websites each person visits
+n4 <- 5 # number ot types of websites / people
 
 # each person visits a total of n3 websites
 # which websites they visit depends on a tuning parameter tau (which controls their randomness)
@@ -33,64 +118,18 @@ audience_tbl <- tibble(
 # when tau is 1 they can visit any website
 # when tau is 0.5, half of the websites they visit can be any website, the other half have to be restricted to those whose outlet_id == p_id
 
-tau <- 0.01
-audience_el <- NULL
-
-# loop over each person
-for(p in 1:1000) {
+set.seed(42)
+simulation_tbl <- NULL
+for(tau in seq(0.8, 0.9, by = 0.001)) {
+  message(tau)
+  g <- get_simulated_network(n1, n2, n3, n4, tau)
+  plot.igraph(g)
+  walktrap.community(g)
   
-  # when tau is 0 all of their choices are selective
-  # when tau is 1 all of their choices are random
-  selective_choices_allowed <- (1-tau) * n3
-  random_choices_allowed <- tau * n3
-  
-
-  selective_chosen_outlets <- outlets_tbl %>%               # only from
-    filter(outlet_type == audience_tbl$p_type[p]) %>%       # those outlets where outlet_type == p_type
-    pull(outlet_id) %>%                                     #
-    sample(selective_choices_allowed, replace = TRUE)       # sample the number of selective outlets allowed
-  
-  random_chosen_outlets <- outlets_tbl %>%                  # from
-    pull(outlet_id) %>%                                     # all outlets in the universe
-    sample(random_choices_allowed, replace = TRUE)          # sample the number of random outlets allowed
-  
-  all_chosen_outlets <- c(selective_chosen_outlets,
-                          random_chosen_outlets)
-  
-  # build the edge-list for the audience network
-  audience_el <- audience_el %>%
-    rbind(
-      tibble(
-        p_name = paste("P", rep(p, n3), sep = "_"),             # one column is the p_id
-        outlet_name = paste("O", all_chosen_outlets, sep = "_") # second column is the outlet_id
-      )
-    )
+  simulation_tbl <- tibble(tau = tau,
+         n_comm = length(walktrap.community(g))
+   ) %>% rbind(simulation_tbl)
 }
 
-audience_g <- graph_from_data_frame(audience_el, directed = F)
-V(audience_g)$type <- substr(V(audience_g)$name, 1, 1) == "O"
 
-projection_graphs <- bipartite_projection(audience_g, multiplicity = TRUE)
-
-outlet_projection <- projection_graphs$proj2
-
-V(outlet_projection)$type <- unlist(
-                                lapply(V(outlet_projection)$name,
-                                  FUN = function(x) { 
-                                    outlets_tbl %>%
-                                      filter(outlet_name == x) %>% 
-                                      pull(outlet_type)
-                                    }
-                                  )
-                              )
-  
-
-# colrs <- c("gray50", "tomato", "gold", "purple", "cyan")
-V(outlet_projection)$color <- ifelse(V(outlet_projection)$type == "A", "gray50",
-                                     ifelse(V(outlet_projection)$type == "B", "tomato",
-                                            ifelse(V(outlet_projection)$type == "C", "gold",
-                                                   ifelse(V(outlet_projection)$type == "D", "olivedrab4",
-                                                          "cyan"))))
-
-plot.igraph(outlet_projection)
 
