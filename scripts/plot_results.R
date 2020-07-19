8cf16dd81640e61d5ea8333ddc748bbcb005c62e
Subhayan Mukerjee <mail@subhayan.com>
2020-07-18 23:56:21 -0400

    model 6 script added

--- scripts/plot_results.R
+++ scripts/plot_results.R
@@ -79,3 +79,24 @@ ggplot(NMI_results) +
   geom_boxplot(aes(x=as_factor(alpha), y=NMI_scores)) +
   facet_wrap(.~method, nrow = 8, ncol = 2) +
   theme_bw()
+
+#################################################################
+
+# plotting NMI w/ scale free
+reqd_files_indices <- list.files("results/") %>%
+  grep(pattern = "CLOUD_NMI_PL_N_100")
+
+reqd_files <- list.files("results/")[reqd_files_indices]
+
+NMI_PL_results <- NULL
+for(file in reqd_files) {
+  NMI_PL_result <- read_csv(paste0("results/", file))
+  NMI_PL_results <- NMI_PL_result %>%
+    rbind(NMI_PL_results)
+}
+
+ggplot(NMI_PL_results) +
+  geom_boxplot(aes(x=as_factor(rho), y=NMI_scores)) +
+  facet_wrap(.~method, nrow = 8, ncol = 2) +
+  theme_bw()
+
